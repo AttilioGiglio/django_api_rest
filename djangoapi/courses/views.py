3b5c453cf182d7ecf84610644120ccdedbd5dbb5
attiliogiglio <attilio.g93@gmail.com>
2020-12-04 15:28:31 -0300

    create views

--- djangoapi/courses/views.py
+++ djangoapi/courses/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Course
 from .serializers import CourseSerializer
+
+class CourseView(viewsets.ModelViewSet):
+    # query to DB, to get all data from table Course DB
+    queryset = Course.objects.all()
+    # Indicate what serializerclass to use. Change data from python code to JSON data
+    serializer_class = CourseSerializer
